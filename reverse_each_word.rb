a4c1a12207d3b6f5eea111499ad7b2e151251a38
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-30 00:44:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,12 @@ require "pry"
 
 def reverse_each_word(string)
   arr_of_words = string.split(" ")
-  binding.pry
-  arr_of_words.collect do |word|
-    word.reverse
+  reversed_word_arr = []
+  
+  arr_of_words.each do |word|
+    reversed_word = word.reverse
+    reversed_word_arr << reversed_word
   end
   
-    
+  reversed_word.join(" ")
 end
